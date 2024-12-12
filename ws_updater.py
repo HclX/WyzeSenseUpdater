4f33a41971ffb49c2b2ca440e35ab66bf74bca89
Xuan Xing <xuanxing@google.com>
2024-12-11 21:54:13 -0800

    finishing functionality, add some firmware dumps

--- ws_updater.py
+++ ws_updater.py
@@ -7,9 +7,7 @@ import errno
 import time
 import struct
 import binascii
-
-logging.basicConfig(level=logging.DEBUG)
-
+import argparse
 
 SBL_CC2650_MAX_MEMREAD_WORDS = 63
 SBL_CC2650_MAX_MEMREAD_BYTES = SBL_CC2650_MAX_MEMREAD_WORDS * 4
@@ -38,6 +36,9 @@ FLASH_START_ADDR = 0x0
 FLASH_SIZE = 0x20000
 PAGE_SIZE = 0x1000
 BL_CFG_OFFSET = 0xFDB
+
+VENDOR_ID = 0x1a86
+PRODUCT_ID = 0xe024
 
 def hexify(data):
     if data and len(data):
@@ -192,13 +193,14 @@ def cmd_status(fd):
 def cmd_reset(fd):
     return dongle_cmd(fd, CMD_RESET)
 
-def cmd_read_mem(fd, addr, size):
+def cmd_read_mem(fd, addr, size, progress_cb=lambda *args: None):
     assert addr & 0x03 == 0, f'Invalid address'
     assert size & 0x03 == 0, f'Invalid size'
     assert size > 0, f'Invalid size'
 
     data = b''
     while size > 0:
+        progress_cb(f'Reading {addr:08X}...')
         chunk_size = size
         if chunk_size > SBL_CC2650_MAX_MEMREAD_BYTES:
             chunk_size = SBL_CC2650_MAX_MEMREAD_BYTES
@@ -213,6 +215,7 @@ def cmd_read_mem(fd, addr, size):
         size -= chunk_size
         addr += chunk_size
 
+    progress_cb('Done', True)
     return data
 
 def cmd_erase(fd, addr, size):
@@ -258,7 +261,7 @@ def cmd_send_data(fd, data):
     
     return True
 
-def cmd_write_range(fd, addr, data):
+def cmd_write_range(fd, addr, data, progress_cb=lambda x,y: None):
     bl_cfg_addr = FLASH_START_ADDR + FLASH_SIZE - PAGE_SIZE + BL_CFG_OFFSET
     assert addr < bl_cfg_addr, f'Invalid address'
     
@@ -266,16 +269,16 @@ def cmd_write_range(fd, addr, data):
     if bl_cfg_idx < len(data):
         assert data[bl_cfg_idx] != 0xC5, f'Invalid bootloader config byte:{data[bl_cfg_idx]:02X}'
 
-    logging.debug(f'Intiating download, addr={addr:08X}, size={len(data):08X}...')
+    progress_cb(f'Intiating download, addr={addr:08X}, size={len(data):08X}...')
 
     if not cmd_download(fd, addr, len(data)):
-        logging.debug(f'Cmd Download failed...')
+        logging.error(f'Cmd Download failed...')
         return False
 
     offset = 0
     retry_count = 0
     while offset < len(data):
-        logging.info(f'Flashing {offset}/{len(data)}...')
+        progress_cb(f'Flashing {offset}/{len(data)}...')
 
         chunk_size = len(data) - offset
         if chunk_size > MAX_PACK_SIZE:
@@ -283,15 +286,15 @@ def cmd_write_range(fd, addr, data):
 
         if not cmd_send_data(fd, data[offset:offset + chunk_size]):
             retry_count += 1
-            logging.info(f'Sending data failed, retrying {retry_count}...')
+            logging.warn(f'Sending data failed, retrying {retry_count}...')
             if retry_count > 3:
-                logging.info(f'Max retry reached...')
+                logging.error(f'Max retry reached...')
                 return False
             continue
 
         retry_count = 0
         offset += chunk_size
-    logging.info("Done.")
+    progress_cb("Done.", True)
 
 def cmd_crc32(fd, addr, size):
     cmd_data = addr.to_bytes(4, byteorder='big') + size.to_bytes(4, byteorder='big') + b'\x00\x00\x00\x00'
@@ -299,33 +302,25 @@ def cmd_crc32(fd, addr, size):
     assert len(rsp) ==4, f'Invalid response'
     return int.from_bytes(rsp, byteorder='big')
 
-def flash_dump(fd, file):
-    logging.info("Reading flash...")
-    rsp = cmd_read_mem(fd, FLASH_START_ADDR, FLASH_SIZE)
-    if not rsp:
-        logging.info(f'Reading flash failed')
-        return
+def show_progress(progress, done=False):
+    sys.stdout.write(f'\r{progress}')
+    sys.stdout.flush()
 
-    assert len(rsp) == FLASH_SIZE, f'Invalid response'
+    if done:
+        sys.stdout.write('\n')
 
-    crc_chip = cmd_crc32(fd, FLASH_START_ADDR, FLASH_SIZE)
-    crc_dump = binascii.crc32(rsp)
-    assert crc_chip == crc_dump, f'Invalid CRC'
+def do_flash(fd, args):
+    logging.info(f"Writing flash content from file {args.file}...")
 
-    with open(file, 'wb') as f:
-        f.write(rsp)
-
-def flash_write(fd, file):
-    data = open(file, 'rb').read()
-
+    data = open(args.file, 'rb').read()
     if len(data) != FLASH_SIZE:
-        logging.info(f'Invalid flash file size')
-        return
+        logging.error(f'Invalid flash file size')
+        return False
 
     bldr_cfg = data[FLASH_SIZE - PAGE_SIZE + BL_CFG_OFFSET]
     if bldr_cfg != 0xC5:
-        logging.info(f'Invalid bootloader config byte:{bldr_cfg:02X}')
-        return
+        logging.error(f'Invalid bootloader config byte:{bldr_cfg:02X}')
+        return False
 
     data = data[:-0x1000]
     data_crc32 = binascii.crc32(data)
@@ -333,222 +328,58 @@ def flash_write(fd, file):
 
     logging.info("Erasing flash...")
     if not cmd_erase(fd, FLASH_START_ADDR, len(data)):
-        logging.info("Erasing flash failed")
-        return
+        logging.error("Erasing flash failed")
+        return False
 
     logging.info("Writing flash...")
-    cmd_write_range(fd, FLASH_START_ADDR, data)
+    if not cmd_write_range(fd, FLASH_START_ADDR, data, progress_cb=show_progress):
+        logging.error("Writing flash failed")
+        return False
 
     chip_crc32 = cmd_crc32(fd, FLASH_START_ADDR, len(data))
     logging.info(f"Chip CRC:0x{chip_crc32:08X}")
+    if chip_crc32 != data_crc32:
+        logging.error("CRC doesn't match!")
+        return False
 
-def main(devPath):
-    fd = dongle_open(devPath)
+    return True
 
-    logging.info("Requesting Upgrade Mode...")
-    req = b'\x07\xAA\x55\x43\x03\x12\x01\x57'
-    rawhid_write(fd, req)
-    rsp = rawhid_read(fd)
-    
-    logging.info("Requesting auto baud...")
-    cmd_auto_baud(fd)
+def do_dump(fd, args):
+    logging.info(f"Reading flash content to {args.file}...")
 
-    logging.info("Sending ping command...")
-    cmd_ping(fd)
+    rsp = cmd_read_mem(fd, FLASH_START_ADDR, FLASH_SIZE, progress_cb=show_progress)
+    if not rsp:
+        logging.info(f'Reading flash failed')
+        return False
 
-    logging.info("Requesting Chip ID...")
-    chipid = cmd_chip_id(fd)
-    logging.info(f'Chip ID: {chipid:04X}')
+    assert len(rsp) == FLASH_SIZE, f'Invalid response'
 
-    logging.info("Reading RAM size...")
-    rsp = cmd_read_mem(fd, 0x40082250, 4)
-    ram_size = int.from_bytes(rsp, byteorder='big')
-    logging.info(f"RamSize={ram_size:08X}")
+    crc_chip = cmd_crc32(fd, FLASH_START_ADDR, FLASH_SIZE)
+    crc_dump = binascii.crc32(rsp)
+    assert crc_chip == crc_dump, f'Invalid CRC'
 
-    logging.info("Reading Flash size...")
-    rsp = cmd_read_mem(fd, 0x4003002c, 4)
-    flash_size = int.from_bytes(rsp, byteorder='big')
-    logging.info(f"RamSize={flash_size:08X}")
+    with open(args.file, 'wb') as f:
+        f.write(rsp)
+    return True
 
-    logging.info("Reading Chip ID...")
-    rsp = cmd_read_mem(fd, 0x50001318, 4)
-    chipid = int.from_bytes(rsp, byteorder='big')
-    logging.info(f"Chipid={chipid:08X}")
+def do_ccfg(fd, args):
+    logging.info(f"Reading CCFG content to {args.file}...")
 
-    action = "write"
-    if action == "dump":
-        flash_dump(fd, 'flash.bin')
-    else:
-        flash_write(fd, 'flash.bin')
-    return
+    rsp = cmd_read_mem(fd, 0x50001000, 0x3F0, progress_cb=show_progress)
+    if not rsp:
+        logging.info(f'Reading flash failed')
+        return False
 
-    """
-        //Get Flash Size
-        Console.WriteLine("Requesting RAM Size");
-        var ramResp = await readMemory32(0x40082250, 1);
+    assert len(rsp) == 0x3F0, f'Invalid response'
 
+    crc_chip = cmd_crc32(fd, 0x50001000, 0x3F0)
+    crc_dump = binascii.crc32(rsp)
+    assert crc_chip == crc_dump, f'Invalid CRC'
 
-        //Get Flash Size
-        Console.WriteLine("Requesting Flash Size");
-        var flashResp = await readMemory32(0x4003002c, 1);
+    with open(args.file, 'wb') as f:
+        f.write(rsp)
+    return True
 
-        Console.WriteLine("Requesting Device ID");
-        var ipDeviceID = await readMemory32(0x50001318, 1);
-
-        //DUMP FLASH FIRMWARE
-        string rwResp;
-        while (true)
-        {
-            Console.WriteLine("Read or Write? <r,w, exit>");
-            rwResp = Console.ReadLine();
-            rwResp = rwResp.ToLower();
-            if (rwResp == "w" || rwResp == "r")
-                break;
-            if (rwResp == "exit")
-                return;
-        }
-
-        if (rwResp == "r")
-        {
-
-            Console.WriteLine("Output name:");
-            string outputPath = Console.ReadLine();
-
-            var flashMem = await readMemory32(0x0, 32768); //32768 * 4 = size of firmware file pulled from my v2 came (and the HMS).
-            Console.WriteLine("Recv all Flash");
-
-            using (var writer = new BinaryWriter(File.OpenWrite(outputPath)))
-            {
-                writer.Write(flashMem.Item2.ToArray());
-            }
-            Console.WriteLine("File saved");
-
-        }
-        else if (rwResp == "w")
-        {
-            string firmwarefile;
-            while (true)
-            {
-                Console.WriteLine("Firmare file?:");
-                firmwarefile = Console.ReadLine();
-                if (File.Exists(firmwarefile))
-                    break;
-                Console.WriteLine("Firmware file not found");
-            }
-
-            Console.WriteLine("***********************");
-            Console.WriteLine("***STARTING TO FLASH***");
-            Memory<byte> newFirmware = new Memory<byte>(File.ReadAllBytes(firmwarefile));
-
-
-    }
-}
-else
-    Console.WriteLine($"Device doesn't exist at path: {args[0]}");
-}
-else
-Console.WriteLine($"No device path supplied './WyzeSenseUpgrade [device path]");
-        }
-        private static async Task<bool> cmdReset()
-        {
-            await sendCmd(0x25, null);
-            return true;
-        }
-
-
-        private static async Task<(bool, uint)> getCRC32(uint Address, uint Length)
-        {
-            Memory<byte> packet = new byte[12];
-            BinaryPrimitives.WriteUInt32BigEndian(packet.Slice(0).Span, Address);
-            BinaryPrimitives.WriteUInt32BigEndian(packet.Slice(4).Span, Length);
-            BinaryPrimitives.WriteUInt32BigEndian(packet.Slice(8).Span, 0);
-
-
-            await sendCmd(0x27, packet);
-
-            Console.WriteLine("[getCRC32] Getting Resp");
-            var resp = await getCmdResponse();
-            if (!resp.Item1 || !resp.Item2) return (false, 0);
-
-            Console.WriteLine("[getCRC32] Getting Resp Data");
-
-
-            var dataResp = await getCmdResponseData(resp.Item3);
-
-            if (!dataResp.Item1)
-            {
-                sendCmdAck(false);
-                return (false, BinaryPrimitives.ReadUInt32BigEndian(dataResp.Item2.Span));
-            }
-
-            Console.WriteLine($"[getCRC32] Raw Resp Data: {DataToString(dataResp.Item2.Span)}");
-
-            sendCmdAck(true);
-            return(true, BinaryPrimitives.ReadUInt32BigEndian(dataResp.Item2.Span));
-        }
-
-        private static uint calcCRC32LikeChip(ReadOnlySpan<byte> Data)
-        {
-            uint d, ind;
-            uint acc = 0xFFFFFFFF;
-            uint[] ulCrcRand32Lut = new uint[16]
-            {
-                 0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
-                0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
-                0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
-                0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
-            };
-            int byteCnt = Data.Length;
-            //for(int idx = 0; idx < Data.Length; idx++)
-            int idx = 0;
-            while(byteCnt-- != 0)
-            {
-                d = Data[idx++];
-                ind = (acc & 0x0F) ^ (d & 0x0F);
-                acc = (acc >> 4) ^ ulCrcRand32Lut[ind];
-                ind = (acc & 0x0F) ^ (d >> 4);
-                acc = (acc >> 4) ^ ulCrcRand32Lut[ind];
-            }
-
-            return (acc ^ 0xFFFFFFFF);
-        }
-        private static byte generateCheckSum(uint Command, ReadOnlyMemory<byte> Data)
-        {
-            int count = (int)Command;
-            ReadOnlySpan<byte> dataSpan = Data.Span;
-
-            for(int i = 0; i < dataSpan.Length; i++)
-            {
-                count += dataSpan[i];
-            }
-            return (byte)count;
-        }
-
-        private static void sendCmdAck(bool Success)
-        {
-            Console.WriteLine($"[sendCmdAck] Sending {(Success ? "Ack" : "Nack")}");
-            Span<byte> buff = new byte[2];
-            //buff[0] = 3;
-            buff[0] = 0;
-            buff[1] =(byte)(Success ? 0xCC : 0x33);
-            
-            dongleStream.Write(buff);
-
-        }
-        private static string DataToString(ReadOnlySpan<byte> data)
-        {
-            string byteString = "";
-            for (int i = 0; i < data.Length; i++)
-            {
-                byteString += string.Format("{0:X2} ", data[i]);
-            }
-            return byteString.TrimEnd(' ');
-        }
-    }
-}
-"""
-VENDOR_ID = 0x1a86
-PRODUCT_ID = 0xe024
 
 def find_bridge_dev():
     # Path to the /dev directory for hidraw devices
@@ -564,10 +395,74 @@ def find_bridge_dev():
             return hidraw_path
     return None
 
+def main():
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Tool for device operations")
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0,
+        help="Increase verbosity level (-v for INFO, -vv for DEBUG)"
+    )
+    parser.add_argument("-d", "--device", type=str, default=find_bridge_dev(), help="Device file path")
+
+    # Subparsers for actions
+    subparsers = parser.add_subparsers(dest="action", required=True)
+
+    # Flash subcommand
+    flash_parser = subparsers.add_parser("flash", help="Flash a file to the device")
+    flash_parser.add_argument(metavar="input", dest="file", type=str, help="File to flash")
+    flash_parser.set_defaults(func=do_flash)
+
+    # Dump subcommand
+    dump_parser = subparsers.add_parser("dump", help="Dump data from the device")
+    dump_parser.add_argument(metavar="output", dest="file", type=str, help="Output file for dumped data")
+    dump_parser.set_defaults(func=do_dump)
+
+    # Dump subcommand
+    dump_parser = subparsers.add_parser("ccfg", help="Dump ccfg from the device")
+    dump_parser.add_argument(metavar="output", dest="file", type=str, help="Output file for dumped data")
+    dump_parser.set_defaults(func=do_ccfg)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Set logging level based on verbosity
+    logging_levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+    logging_level = logging_levels[min(args.verbose, len(logging_levels) - 1)]
+    logging.basicConfig(level=logging_level)
+
+    if not args.device:
+        logging.error(f'No wyze sense bridge device file specified or found!')
+        return -1
+    
+    logging.info(f'Using bridge device {args.device}...')
+    fd = dongle_open(args.device)
+
+    logging.info("Requesting Upgrade Mode...")
+    req = b'\x07\xAA\x55\x43\x03\x12\x01\x57'
+    rawhid_write(fd, req)
+    rawhid_read(fd)
+
+    logging.info("Requesting auto baud...")
+    if not cmd_auto_baud(fd):
+        logging.error(f'cmd_auto_baud failed')
+        return -1
+
+    logging.info("Sending ping command...")
+    if not cmd_ping(fd):
+        logging.error(f'cmd_ping failed')
+
+    logging.info("Requesting Chip ID...")
+    chipid = cmd_chip_id(fd)
+    logging.info(f'Chip ID: {chipid:04X}')
+
+    # Call the appropriate function
+    if not args.func(fd, args):
+        logging.error(f'Operation failed')
+        return -1
+
+    logging.info('Done!')
+    cmd_reset(fd)
+    return 0
+
 if __name__ == "__main__":
-    bridge_dev = find_bridge_dev()
-    if bridge_dev:
-        logging.info(f'Openning device {bridge_dev}...')
-        main(bridge_dev)
-    else:
-        logging.info('No wyzesense device found...')
+    main()
